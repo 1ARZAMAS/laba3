743381173d0e0dff3f3914e7cfa9132b82e132bd
1ARZAMAS <lizaakutkina@gmail.com>
2024-04-14 13:07:03 +0700

    added graph

---
+++ task1.cpp
@@ -0,0 +1,62 @@
+#include <iostream> 
+#include <cmath> 
+#include <iomanip> 
+#include <fstream> 
+ 
+using namespace std; 
+ 
+double firstFragm(double x){ 
+    double res = - sqrt(-x*x - 4*x)+2;
+    if (abs(res) < 0.000001) { 
+        res = 0.0; 
+    } 
+    return res; 
+} 
+ 
+double secondFragm(double x){ 
+    double res = 0.0; 
+    return res; 
+} 
+ 
+double thirdFragm(double x){ 
+    double res = -x;
+    return res; 
+} 
+ 
+double fourthFragm(double x){ 
+    double res = sqrt(-x*x + 6*x - 5)-1;
+    if (abs(res) < 0.000001) { 
+        res = 0.0; 
+    } 
+    return res; 
+} 
+ 
+int main () { 
+    double xStart = -4.0; 
+    double xEnd = 5.0; 
+    double dx = 0.5; 
+ 
+    ofstream outfile("test.txt"); 
+    if (!outfile.is_open()) { 
+        cout << "При открытии файла произошла ошибка" << endl; 
+        return 1; 
+    } 
+    outfile << setw(10)  << "x" << setw(10) << "y" << endl;
+    for (double x = xStart; x <= xEnd ; x += dx){ 
+        if (abs(x) < 0.000001) { 
+        x = 0.0; 
+        } 
+        if (x >= -4.0 && x < -2.0) { 
+            outfile << setw(10)  << x << setw(10) << firstFragm(x) << endl; 
+        } 
+        if (x >= -2.0 && x < 0.0){ 
+            outfile << setw(10)  << x << setw(10) << secondFragm(x) << endl; 
+        } 
+        if (x >= 0.0 && x < 3.0){ 
+            outfile << setw(10)  << x << setw(10) << thirdFragm(x) << endl; 
+        } 
+        if (x >= 3.0 && x <= 5.0){ 
+            outfile << setw(10)  << x << setw(10) << fourthFragm(x) << endl; 
+        } 
+    } 
+}
